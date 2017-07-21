af685dd48bafd053399842d471a7d929d6c13006
RR <heyrutvik@gmail.com>
2017-07-21 09:45:30 +0530

    simple parser implemented

--- src/main/scala/ch33/LogicalExpr.scala
+++ src/main/scala/ch33/LogicalExpr.scala
@@ -4,24 +4,25 @@ import scala.util.parsing.combinator._
 
 class LogicalExprGrammer extends JavaTokenParsers {
 
-  val m = Map("1" -> "hello", "2" -> "world", "3" -> "silence")
+  val m = Map("1" -> "x > 2", "2" -> "y <= 50", "3" -> "z == false")
 
   lazy val expr: Parser[String] = term ~ rest ^^ {
-    case "(" ~ h ~ ")" ~ t => "(" + m.get(h.asInstanceOf[String]).getOrElse(h.asInstanceOf[String]) + ")" + t
-    case h ~ t => {
-      m.get(h.asInstanceOf[String]).getOrElse(h.asInstanceOf[String]) + t
-    }
+    case h ~ t => h + t
   }
   lazy val rest: Parser[String] = rep(and | or) ^^ {
     case ls => ls.mkString(" ")
   }
   lazy val and: Parser[String] = "and" ~ term ^^ {
-    case _ ~ t => " and " + m.get(t.asInstanceOf[String]).getOrElse(t.asInstanceOf[String])
+    case _ ~ t => " and " + t
   }
   lazy val or: Parser[String] = "or" ~ term ^^ {
-    case _ ~ t => " or " + m.get(t.asInstanceOf[String]).getOrElse(t.asInstanceOf[String])
+    case _ ~ t => " or " + t
   }
-  lazy val term: Parser[Any] = "(" ~ expr ~ ")" | decimalNumber
+  lazy val term: Parser[String] = "(" ~ expr ~ ")" ^^ {
+    case "(" ~ h ~ ")" => "(" + h + ")"
+  } | decimalNumber ^^ {
+    case k => m(k)
+  }
 }
 
 // (new ch33.LogicalExpr()).apply("(1 and 2) or 3")
